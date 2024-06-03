1bbad5c31d15f2e10c35ad9c69d853e02b5e5ae2
Pauloper1 <paulcrescent.p@gmail.com>
2024-06-04 01:32:00 +0530

    navbar css changes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ export const Navbar = () => {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                <div class="container-fluid navbarWrapper">
+                    <a class="navbar-brand" href="#">uiAid</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
